fa6fa73d0739a1adb2ea0229e1994d46b50f52fd
Rik Huijzer <github@huijzer.xyz>
2025-04-30 19:16:06 +0200

    refactor: move commit into sh

--- backup.sh
+++ backup.sh
@@ -4,9 +4,30 @@ set -euxo pipefail
 
 ARCHIVE_PATH="all.tar.xz"
 
-curl --proto "=https" --tlsv1.2 -sSf \
-  -H "Authorization: Bearer $FX_PASSWORD" \
-  https://fx.huijzer.xyz/api/download/all.tar.xz > "$ARCHIVE_PATH"
+download() {
+  curl --proto "=https" --tlsv1.2 -sSf \
+    -H "Authorization: Bearer $FX_PASSWORD" \
+    https://fx.huijzer.xyz/api/download/all.tar.xz > "$ARCHIVE_PATH"
 
-tar --verbose -xf "$ARCHIVE_PATH"
-rm "$ARCHIVE_PATH"
+  tar --verbose -xf "$ARCHIVE_PATH"
+  rm "$ARCHIVE_PATH"
+}
+
+commit() {
+  if [ -n "$(git status --porcelain)" ]; then
+    git config --global user.email "$GITHUB_ACTOR@users.noreply.github.com"
+    git config --global user.name "$GITHUB_ACTOR"
+
+    git add .
+    git commit -m '[bot] backup'
+    git push
+  fi
+}
+
+if [[ "$1" == "download" ]]; then
+  echo "Running download..."
+  download
+elif [[ "$1" == "commit" ]]; then
+  echo "Running commit..."
+  commit
+fi
